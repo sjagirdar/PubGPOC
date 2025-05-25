ee3389d4b36d4fd769c112db89c1852f9bcce515
Swapneel Jagirdar <sjagirdar@gmail.com>
2025-05-25 14:58:40 +0530

    New Asset

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PubGPOC
+
+Hosted at : https://dashboard.render.com/web/srv-d0p3juumcj7s73doojt0/deploys/dep-d0p3naumcj7s73doreu0
+https://pubgpoc.onrender.com/
--- public/script.js
+++ public/script.js
@@ -3,14 +3,14 @@ const imgB = document.getElementById('imgB');
 const button = document.getElementById('next-btn');
 
 let currentIndex = 1;
-const maxImages = 200;
+const maxImages = 18;
 
 let activeImg = imgA;
 let hiddenImg = imgB;
 
 // Returns path to image file
 function getImagePath(index) {
-  return `/images/${index}.png`;
+  return `/images/set2/${index}.png`;
 }
 
 // Preload image and call callback when loaded
